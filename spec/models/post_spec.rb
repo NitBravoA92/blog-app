dbf5f20cdf8ab159d44892ba8490dae18c63341f
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-20 15:54:06 -0400

    Test the most_recent_comments of the Post model when there is 5 comment for a given post.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -13,5 +13,18 @@ RSpec.describe Post, type: :model do
 
       expect(recent_comments).to eq(expected_results)
     end
+
+    it 'should return the last 5 comments added on a post' do
+      first_comment = Comment.create(post: @post, author: @user, text: 'This is the first comment of Mark Miller')
+      second_comment = Comment.create(post: @post, author: @user, text: 'This is the second comment of Mark Miller')
+      third_comment = Comment.create(post: @post, author: @user, text: 'This is the third comment of Mark Miller')
+      fourth_comment = Comment.create(post: @post, author: @user, text: 'This is the fourth comment of Mark Miller')
+      fifth_comment = Comment.create(post: @post, author: @user, text: 'This is the fifth comment of Mark Miller')
+
+      recent_comments = @post.most_recent_comments
+      expected_results = [fifth_comment, fourth_comment, third_comment, second_comment, first_comment]
+
+      expect(recent_comments).to eq(expected_results)
+    end
   end
 end
