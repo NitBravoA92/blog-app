27ad9afc3b032952942490bbd9503f0897bb6224
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-20 16:32:26 -0400

    Fix some linters issues

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   before :each do
     @user = User.create(name: 'Mark Miller', bio: 'I am Mark Miller and I am a Backend Developer')
-    @post = Post.create(author: @user, title: 'My first post about Backend technologies', text: 'This is the first post of Mark Miller')
+    @post = Post.create(author: @user, title: 'My first post about Backend technologies',
+                        text: 'This is the first post of Mark Miller')
   end
 
   describe '#most_recent_comments' do
@@ -30,7 +31,8 @@ RSpec.describe Post, type: :model do
 
   describe '#update_posts_counter_by_user' do
     it 'should update the posts_counter attribute of the author of a post' do
-      second_post = Post.create(author: @user, title: 'My second post about Backend technologies', text: 'This is the second post of Mark Miller')
+      second_post = Post.create(author: @user, title: 'My second post about Backend technologies',
+                                text: 'This is the second post of Mark Miller')
 
       second_post.update_posts_counter_by_user
       expected_results = 2
@@ -47,7 +49,6 @@ RSpec.describe Post, type: :model do
     end
 
     it 'The Post should not be valid when the title has more than 250 characters' do
-      long_post_title = 'Third post title' * 16 
       @post.title = nil
 
       expect(@post).to_not be_valid
