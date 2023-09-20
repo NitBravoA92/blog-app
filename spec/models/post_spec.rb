82a2db46fc1cd63473d45666dd734994ee631b10
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-20 16:28:17 -0400

    Create the test case to validate when the comments_counter is greater than or equal to 0

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -65,6 +65,12 @@ RSpec.describe Post, type: :model do
       expect(@post).to_not be_valid
     end
 
+    it 'The Post should be valid when the comments_counter is greater than or equal to 0' do
+      @post.comments_counter = 13
+
+      expect(@post).to be_valid
+    end
+
     it 'The Post should not be valid when the likes_counter attribute is not an integer' do
       @post.likes_counter = 'new string'
 
