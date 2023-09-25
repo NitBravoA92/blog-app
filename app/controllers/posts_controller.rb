6addba6854bcca439bb3a432600ae0dee9d84210
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-25 17:50:55 -0400

    Create the show action in the Posts controller to display a post content

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,15 @@ class PostsController < ApplicationController
       redirect_to users_url, notice: "User not found"
     end
   end
+
+  def show
+    begin
+      @user_id = params[:user_id]
+      @post_id = params[:id]
+      @users_posts = User.find(@user_id).posts
+      @post = @users_posts.find(@post_id)
+    rescue ActiveRecord::RecordNotFound => e
+      redirect_to user_posts_url, notice: "Post not found"
+    end
+  end
 end
