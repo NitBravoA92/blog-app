9f27e621f592879bcb9a0b4c55d6c88d33474158
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-25 15:08:42 -0400

    Create the route for the Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Adding nested resources routes for users and posts
+  resources :users 
 
   # Defines the root path route ("/")
   # root "articles#index"
