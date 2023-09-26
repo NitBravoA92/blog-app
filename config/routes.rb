477fc888e83796f9e45a7a9571516b1cd1606410
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-25 21:02:13 -0400

    Set only the index and show routes for Users and Posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Adding nested resources routes for users and posts
-  resources :users do
-    resources :posts
+  resources :users , only: [:index, :show] do
+    resources :posts, only: [:index, :show]
   end 
 
   # Defines the root path route ("/")
