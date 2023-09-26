287b43f0ac6260194c54135b38a1407ea848d2db
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-25 22:38:31 -0400

    Test the success response of the index request in Posts controller

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,9 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  before :each do
+    @user = User.create(name: 'William Brown', bio: 'Hi! I am William and I am Frontend Developer')
+    @post = Post.create(author: @user, title: 'Post #1', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+  end
+
   describe 'GET /index' do
+    before :each do
+      @user_url_posts = user_posts_url(@user)
+    end
+
+    # test if the response status was correct (status 200).
     it 'returns http success' do
-      get '/posts/index'
+      get @user_url_posts
       expect(response).to have_http_status(:success)
     end
   end
