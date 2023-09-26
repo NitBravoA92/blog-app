953585462548481ec7bd88213067f8228711b4f9
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-25 22:00:49 -0400

    Test the success response of the index request in Users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  before :each do
+    @user = User.create(name: 'William Brown', bio: 'Hi! I am William and I am Frontend Developer')
+    @user_url = users_url
+  end
+
   describe 'GET /index' do
+    # test if the response status was correct (status 200).
     it 'returns http success' do
-      get '/users/index'
+      get @user_url
       expect(response).to have_http_status(:success)
     end
   end
