6f973ff27c139406eb7991e6e83c05953103b9e3
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-27 02:02:50 -0400

    Refactor the posts controller to improve the code performance

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,14 @@
 class PostsController < ApplicationController
   def index
-    @user_id = params[:user_id]
-    @posts = User.find(@user_id).posts
-    @user_name = 'William Brown'
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
   rescue ActiveRecord::RecordNotFound
     redirect_to users_url, notice: 'User not found'
   end
 
   def show
-    @user_id = params[:user_id]
-    @post_id = params[:id]
-    @users_posts = User.find(@user_id).posts
-    @post = @users_posts.find(@post_id)
+    @users_posts = User.find(params[:user_id]).posts
+    @post = @users_posts.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to user_posts_url, notice: 'Post not found'
   end
