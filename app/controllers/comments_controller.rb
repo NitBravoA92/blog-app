cd62c794227c584ebdd1793a2d3855403f76dbec
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-27 18:15:22 -0400

    Create the new method in the Comments controller to render the Create a comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,10 @@
 class CommentsController < ApplicationController
   def new
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new
+  end
+
+  def create
   end
 end
