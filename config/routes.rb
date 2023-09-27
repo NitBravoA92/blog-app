501034b206ef652786f09d0b69e6a23cb1b94d37
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-27 15:17:28 -0400

    Update the posts routes to process create-new-post request

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Adding nested resources routes for users and posts
   resources :users , only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :new, :create, :show]
   end 
 
   # Defines the root path route ("/")
