009cb69791bc80e7e4aa13ac5aac61743ce09cfb
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-27 18:49:26 -0400

    Create the comment_params method in Comments controller to get the data from the Create comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,10 @@ class CommentsController < ApplicationController
 
   private
 
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
   def find_post_by_id
     Post.find(params[:post_id])
   end
