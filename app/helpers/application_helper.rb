6cb80e7f329f7e6d7ab0c60c4ab984a13e6ee89f
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-27 02:21:39 -0400

    Create the flash_alert method to display an alert in the views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def flash_alert
+    content = ''
+    flash.each do |key, value|
+      content << "
+      <div class='flash_alert-#{key}'>
+        <p class='flash_alert_content'>#{value}</p>
+      </div>
+    "
+    end
+    content.html_safe
+  end
 end
