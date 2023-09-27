8183d390e2196f23b3360d71511543a3125fd9f1
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-27 18:14:46 -0400

    Create the comments routes using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Adding nested resources routes for users and posts
   resources :users , only: [:index, :show] do
-    resources :posts, only: [:index, :new, :create, :show]
+    resources :posts, only: [:index, :new, :create, :show] do
+      resources :comments, only: [:new, :create]
+    end
   end 
 
   # Defines the root path route ("/")
