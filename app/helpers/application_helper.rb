6e18f2584fb084988c28d2bda3238c7ff7c2394b
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-27 03:22:11 -0400

    Create the navigation_path helper method to manage the navigation links of the menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 module ApplicationHelper
+  def navigation_path
+    content = ''
+    if controller_name == 'users' && action_name == 'show'
+      content << "<li>#{link_to 'All Users', users_path }</li>"
+    end
+    if controller_name == 'posts' && action_name == 'index'
+      content << "<li>#{link_to 'User details', user_path(@user) }</li>"
+    end
+    if controller_name == 'posts' && action_name == 'show'
+      content << "<li>#{link_to 'User posts', user_posts_path }</li>"
+    end
+    content.html_safe
+  end
+
   def flash_alert
     content = ''
     flash.each do |key, value|
