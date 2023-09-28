b2fe1952b72d868eb3e18ddc40696422d66a3807
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-27 23:49:13 -0400

    Add the addlike and deletelike routes to manage the post likes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Adding nested resources routes for users and posts
   resources :users , only: [:index, :show] do
     resources :posts, only: [:index, :new, :create, :show] do
+      post 'addlike', on: :member
+      delete 'deletelike', on: :member
       resources :comments, only: [:new, :create]
     end
   end 
