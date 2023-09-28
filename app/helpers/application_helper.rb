8135a56908097a827087ee31e897d316a8830ef1
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-27 20:22:14 -0400

    Create the generate_nav_link method in application helper to improve the navigation code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,13 +5,17 @@ module ApplicationHelper
       content << "<li>#{link_to 'All Users',
                                 users_path}</li>"
     end
-    if controller_name == 'posts' && action_name == 'index'
-      content << "<li>#{link_to 'User bio', user_path(@user)}</li>"
-    end
-    if controller_name == 'posts' && action_name == 'show'
-      content << "<li>#{link_to 'User posts', user_posts_path}</li>"
-    end
+    content << generate_nav_link({ name: 'posts', action: 'index' }, 'User bio', user_path(@user))
+    content << generate_nav_link({ name: 'posts', action: 'new' }, 'User posts', user_posts_path)
+    content << generate_nav_link({ name: 'posts', action: 'show' }, 'User posts', user_posts_path)
+    content << generate_nav_link({ name: 'comments', action: 'new' }, 'Back to post', user_post_path(@user, @post))
     content.html_safe
+  end
+
+  def generate_nav_link(req_data, link_title, link_url)
+    return '' unless controller_name == req_data[:name] && action_name == req_data[:action]
+
+    "<li>#{link_to link_title, link_url}</li>"
   end
 
   def flash_alert
