cdafaea381a0f83d9fdb24ebc28043bba779ba59
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-27 21:33:59 -0400

    Create the get_url and generate_nav_link methods to improve navigation functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,19 +3,29 @@ module ApplicationHelper
     content = ''
     unless controller_name == 'users' && action_name == 'index'
       content << "<li>#{link_to 'All Users',
-                                users_path}</li>"
+      get_url(1)}</li>"
     end
-    content << generate_nav_link({ name: 'posts', action: 'index' }, 'User bio', user_path(@user))
-    content << generate_nav_link({ name: 'posts', action: 'new' }, 'User posts', user_posts_path)
-    content << generate_nav_link({ name: 'posts', action: 'show' }, 'User posts', user_posts_path)
-    content << generate_nav_link({ name: 'comments', action: 'new' }, 'Back to post', user_post_path(@user, @post))
+
+    content << generate_nav_link({ name: 'posts', action: 'index' }, 'User bio', 2)
+    content << generate_nav_link({ name: 'posts', action: 'new' }, 'User posts', 3)
+    content << generate_nav_link({ name: 'posts', action: 'show' }, 'User posts', 3)
+    content << generate_nav_link({ name: 'comments', action: 'new' }, 'Back to post', 4)
     content.html_safe
   end
 
-  def generate_nav_link(req_data, link_title, link_url)
+  def generate_nav_link(req_data, title, level)
     return '' unless controller_name == req_data[:name] && action_name == req_data[:action]
 
-    "<li>#{link_to link_title, link_url}</li>"
+    "<li>#{link_to title, get_url(level)}</li>"
+  end
+
+  def get_url(level)
+    case level
+    when 1 then users_path
+    when 2 then user_path(@user)
+    when 3 then user_posts_path
+    when 4 then user_post_path(@user, @post)
+    end
   end
 
   def flash_alert
