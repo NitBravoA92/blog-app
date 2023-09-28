a1323a77aa1b10a74e5c1ef205cacce8113d13eb
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-28 00:45:58 -0400

    Replace the link_to helper by button_to helper to process the requests for likes

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,9 +6,9 @@ module PostsHelper
 
   def show_likes_button
     if @is_liked
-      link_to "Don't Like it", deletelike_user_post_path(@post.author, @post), method: :delete, class: 'btn'
+      button_to "Don't Like it", deletelike_user_post_path(@post.author, @post), method: :delete, class: 'btn', remote: true
     else
-      link_to 'Like', addlike_user_post_path(@post.author, @post), method: :post, class: 'btn'
+      button_to 'Like it', addlike_user_post_path(@post.author, @post), method: :post, class: 'btn', remote: true
     end 
   end
 end
