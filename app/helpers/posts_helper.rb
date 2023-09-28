eea4838f09ae33a5d992892a136c3a176e95997a
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-28 00:01:26 -0400

    Create the show_likes_button helper method to render a like or dislike button

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,4 +3,12 @@ module PostsHelper
     size = 100
     text.length <= size ? text : "#{text[0..size]}..."
   end
+
+  def show_likes_button
+    if @is_liked
+      link_to "Don't Like it", deletelike_user_post_path(@post.author, @post), method: :delete, class: 'btn'
+    else
+      link_to 'Like', addlike_user_post_path(@post.author, @post), method: :post, class: 'btn'
+    end 
+  end
 end
