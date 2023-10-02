8a86cf2b3cda95e46c76543df8c3fb81fae09ebf
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-02 18:24:24 -0400

    Test if the number of posts of the user is displayed correctly in the show page of users

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -16,6 +16,15 @@ describe "Visit the show page of 'users'", type: :feature do
     expect(page).to have_selector('img')
 
     expect(page).to have_css('img[src="https://randomuser.me/api/portraits/men/88.jpg"]')
-    expect(page).to have_css('img[alt="Frank Miller photo"]')
+    expect(page).to have_css('img[alt="Frank Miller photo"]', count: 1)
+  end
+
+  it "should display the number of posts the user has written" do
+    post1 = Post.create(author: @user, title: 'Post 1', text: 'This is the content of Post 1')
+    post2 = Post.create(author: @user, title: 'Post 2', text: 'This is the content of Post 2')
+
+    visit user_path(@user)
+
+    expect(page).to have_content 'Number of posts: 2'
   end
 end
