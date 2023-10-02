b4d098904cf122c5e6b371a33feba39880f00f23
Cesar Herrera <cesarherr321@gmail.com>
2023-10-02 17:43:29 -0300

    Add test for profile picture

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-require 'rails_helper'
 describe "Visit the index page of 'users'", type: :feature do
   before :each do
     @first_user = User.create(name: 'Frank Miller', photo: 'https://randomuser.me/api/portraits/men/88.jpg', bio: 'Hello! I am a Ruby Backend Developer from USA.')
@@ -15,5 +14,15 @@ describe "Visit the index page of 'users'", type: :feature do
     expect(page).to have_content 'Peter Williams'
   end
 
-  
+  it 'should display the profile picture of each user' do
+    visit users_path
+    expect(page).to have_selector('img')
+    expect(page).to have_css('img[src="https://randomuser.me/api/portraits/men/88.jpg"]')
+    expect(page).to have_css('img[alt="Frank Miller photo"]')
+    expect(page).to have_css('img[src="https://randomuser.me/api/portraits/men/90.jpg"]')
+    expect(page).to have_css('img[alt="Jason Rogers photo"]')
+    expect(page).to have_css('img[src="https://randomuser.me/api/portraits/men/32.jpg"]')
+    expect(page).to have_css('img[alt="Peter Williams photo"]')
+  end
+
 end
