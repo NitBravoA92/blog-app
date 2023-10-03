8c9581536bc30ad030817d5bd9fccab61b5d4873
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-03 19:53:07 -0400

    Add the SMTP configuration to test the confirmation emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,11 +36,15 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
+  
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
