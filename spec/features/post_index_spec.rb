283c35dc3bfdec6255897eff354b8197da487e03
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-02 20:04:48 -0400

    Test if the user picture is displayed correctly in the index page of posts

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -10,4 +10,12 @@ describe "Visit the index page of 'posts'", type: :feature do
     expect(page).to have_content 'Jason Rogers'
   end
 
+  it 'should display the profile picture of the user' do
+    visit user_posts_path(@user)
+
+    expect(page).to have_selector('img')
+
+    expect(page).to have_css('img[src="https://randomuser.me/api/portraits/men/90.jpg"]')
+    expect(page).to have_css('img[alt="Jason Rogers photo"]', count: 1)
+  end
 end
