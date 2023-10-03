46a46f91f9d1b63f0ecf605e8239746ba8b4ff58
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-02 20:06:15 -0400

    Test if the Number of posts of a user is displayed correctly in the index page of posts

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -18,4 +18,14 @@ describe "Visit the index page of 'posts'", type: :feature do
     expect(page).to have_css('img[src="https://randomuser.me/api/portraits/men/90.jpg"]')
     expect(page).to have_css('img[alt="Jason Rogers photo"]', count: 1)
   end
+
+  it "should display the number of posts the user has written" do
+    post1 = Post.create(author: @user, title: 'Post 1', text: 'This is the content of Post 1')
+    post2 = Post.create(author: @user, title: 'Post 2', text: 'This is the content of Post 2')
+    post3 = Post.create(author: @user, title: 'Post 3', text: 'This is the content of Post 3')
+
+    visit user_posts_path(@user)
+
+    expect(page).to have_content 'Number of posts: 3'
+  end
 end
