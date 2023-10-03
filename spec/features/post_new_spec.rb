e96c12bf8f95b5a52c5833f62fa3d7729e255800
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-02 23:23:59 -0400

    Test if the form of creating a post is submitted successfully - new page of posts

--- spec/features/post_new_spec.rb
+++ spec/features/post_new_spec.rb
@@ -16,4 +16,15 @@ describe "Visit the new page of 'posts'", type: :feature do
     expect(page).to have_content 'Title'
     expect(page).to have_content 'Text'
   end
+
+  it "should create a new Post when user click on 'Save post' button" do
+    visit new_user_post_path(@user)
+
+    within("form") do
+      fill_in 'Title', with: 'Post 3'
+      fill_in 'Text', with: 'This is the content of Post 3'
+    end
+    click_button 'Save post'
+    expect(page).to have_current_path(user_posts_path(@user))
+  end
 end
