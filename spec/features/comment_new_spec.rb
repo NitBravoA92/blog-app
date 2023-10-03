35b489a46ab4dcb0d465f8a858ac755bf801693d
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-02 23:38:59 -0400

    Test if the form of creating a comment is submitted successfully - new page of comments

--- spec/features/comment_new_spec.rb
+++ spec/features/comment_new_spec.rb
@@ -16,4 +16,14 @@ describe "Visit the new page of 'comments'", type: :feature do
     visit new_user_post_comment_path(@user, @post)
     expect(page).to have_content 'Text'
   end
+
+  it "should create a new comment in a post when the user clicks on 'Save comment' button" do
+    visit new_user_post_comment_path(@user, @post)
+
+    within('form') do
+      fill_in 'Text', with: 'This is the second comment on Post 3'
+    end
+    click_button 'Save comment'
+    expect(page).to have_current_path(user_post_path(@user, @post))
+  end
 end
