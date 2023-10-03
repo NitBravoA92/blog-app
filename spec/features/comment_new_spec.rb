eb6577efa6aeb22b719c6cd7796b87eb301a76bb
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-02 23:33:26 -0400

    Test if the page's title is displayed correctly - new page of comments

---
+++ spec/features/comment_new_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe "Visit the new page of 'comments'", type: :feature do
+  before :each do
+    @user = User.create(name: 'Mark Jhons', photo: 'https://randomuser.me/api/portraits/men/90.jpg',
+                        bio: 'Hello! I am a Web developer from Australia.')
+    @post = Post.create(author: @user, title: 'Post 3', text: 'This is the content of Post 3')
+  end
+
+  it "should display the page title: 'Create a new comment'" do
+    visit new_user_post_comment_path(@user, @post)
+    expect(page).to have_content 'Create a new comment'
+  end
+end
