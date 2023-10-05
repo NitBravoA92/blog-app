90d608efc76d7766ace7b8274fc07bcbf3354c8a
Cesar Herrera <cesarherr321@gmail.com>
2023-10-05 09:40:16 -0300

    Add radio buttons to select the user role on sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :role])
   end
 
   def configure_devise_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user|
-      user.permit(:name, :email, :password, :password_confirmation)
+      user.permit(:name, :email, :password, :password_confirmation, :role)
     end
   end
 end
