4ec02084f8bf6c1bd890278ee0eaa9e2f42596bb
Cesar Herrera <cesarherr321@gmail.com>
2023-10-05 09:35:57 -0300

    Update ability to authorize destroy

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,6 +8,7 @@ class Ability
     return unless user.present?
 
     can :delete, [Post, Comment, Like], author_id_id: user.id
+    can :destroy, [Comment], users_id: user.id
 
     # if the user is admin then return the ability object instead of returning an error
     return unless user.role == 'admin'
