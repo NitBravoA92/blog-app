cf822590edb4ce478f00a778f04127de4626dee5
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-04 21:20:32 -0400

    Add the logic to delete a post in the posts controller, routes.rb file and post model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Adding nested resources routes for users and posts
   resources :users , only: [:index, :show] do
-    resources :posts, only: [:index, :new, :create, :show] do
+    resources :posts, only: [:index, :new, :create, :show, :destroy] do
       post 'addlike', on: :member
       delete 'deletelike', on: :member
       resources :comments, only: [:new, :create]
