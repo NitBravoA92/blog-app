b9add1face907362c5aaa73e0de58161f303589e
Cesar Herrera <cesarherr321@gmail.com>
2023-10-05 09:31:54 -0300

    Add a helper method to comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,12 @@
 module CommentsHelper
+  def show_delete_comment_button(comment)
+    content = ''
+
+    return '' unless can?(:destroy, comment)
+
+    content << button_to('Delete this comment', user_post_comment_path(comment.post.author, comment.post, comment),
+                         method: :delete, class: 'btn', remote: true).to_s
+
+    content.html_safe
+  end
 end
