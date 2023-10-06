b43575c5502eea1c0eee3357d847343d588e2f11
Cesar Herrera <cesarherr321@gmail.com>
2023-10-05 21:01:57 -0300

    Create index Action on CommentsController

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,3 +1,17 @@
 class Api::V1::CommentsController < ApplicationController
 
+  def index
+    user = User.find(params[:user_id])
+    post = user.posts.find(params[:post_id])
+    comments = post.comments
+    render json: comments
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'User not found' }, status: 404
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
