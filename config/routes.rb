26e8dc3465f9a486636d9a838d858343037106ea
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-06 17:41:59 -0400

    Change the routes of the Rswag documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => '/api-documentation'
+  mount Rswag::Api::Engine => '/api-documentation'
   devise_for :users
   # Adding nested resources routes for users and posts
   resources :users , only: [:index, :show] do
