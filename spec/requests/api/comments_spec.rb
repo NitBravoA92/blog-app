36802f44c202c28c46e37acd674e2bbd951be045
Carmen Bravo <nbravoalcala@gmail.com>
2023-10-06 18:30:38 -0400

    Fix some linters issues

--- spec/requests/api/comments_spec.rb
+++ spec/requests/api/comments_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'Api::Comments', type: :request do
   path '/api/v1/users/{user_id}/posts/{post_id}/comments' do
-
     # Get a list of comments for a post with the specified parameters (user_id and post_id)
     get 'Retrieves a list of comments for a post' do
       tags 'Comments'
@@ -30,7 +29,9 @@ RSpec.describe 'Api::Comments', type: :request do
         run_test!
       end
     end
+  end
 
+  path '/api/v1/users/{user_id}/posts/{post_id}/comments' do
     # Save a new comment on a post with the specified parameters (user_id, post_id and text)
     post 'Create a comment on a post' do
       tags 'Comment'
@@ -38,18 +39,18 @@ RSpec.describe 'Api::Comments', type: :request do
 
       parameter name: :user_id, in: :path, type: :string, description: "The user's id that written the posts"
       parameter name: :post_id, in: :path, type: :string, description: "The post's id that has the comments"
-      parameter name: :text, in: :body, type: :string, description: "Content of the comment"
+      parameter name: :text, in: :body, type: :string, description: 'Content of the comment'
 
       # Status code 201 - the comment was successfully created
       response '201', 'Comment created' do
         schema type: :object,
-        properties: {
-          id: { type: :integer },
-          users_id: { type: :integer },
-          posts_id: { type: :integer },
-          text: { type: :string }
-        },
-        required: %w[id user_id post_id text]
+               properties: {
+                 id: { type: :integer },
+                 users_id: { type: :integer },
+                 posts_id: { type: :integer },
+                 text: { type: :string }
+               },
+               required: %w[id user_id post_id text]
 
         let(:user_id) do
           User.create(name: 'James', email: 'james@gmail.com', encrypted_password: '78548dhfudygufydg74').id
